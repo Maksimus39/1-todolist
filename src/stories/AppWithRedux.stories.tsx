3efed6b3c577ed47ef0cc7f13b8781f4e630b61e
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-06-19 13:07:18 +0300

    история компоненты в сторибуке

---
+++ src/stories/AppWithRedux.stories.tsx
@@ -0,0 +1,23 @@
+import AppWithRedux from "../AppWithRedux";
+import {Meta, StoryObj} from "@storybook/react";
+import {ReduxStoreProviderDecorator} from "../state/ReduxStoreProviderDecorator";
+
+
+const meta: Meta<typeof AppWithRedux> = {
+    title: 'TODOLISTS/AppWithRedux',
+    component: AppWithRedux,
+    parameters: {
+        layout: 'centered'
+    },
+    tags: ['autodocs'],
+    decorators: [ReduxStoreProviderDecorator]
+}
+
+export default meta
+
+type Story = StoryObj<typeof AppWithRedux>
+
+export const AppWithReduxStory: Story = {
+    render: () => <AppWithRedux/>
+}
+
